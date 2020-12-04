2729382f82a02bcaf24fd1f8947434a80331e823
abhay-tank <abhay.tank@raweng.com>
2020-12-04 12:32:59 +0530

    frequency counter solved

--- questions/frequencyCounter.js
+++ questions/frequencyCounter.js
@@ -6,6 +6,21 @@ Count the frequency of a vowels in a given string.
 3. In case numbers are passed it shoul throw an error with message "Invalid Input"
 */
 
-const frequencyCounter = (str) => {};
+const frequencyCounter = (str) => {
+   if ((typeof str == "string" || typeof str == "String") && str.length) {
+      let string = str.toLowerCase();
+      let vowelsResult = { a: 0, e: 0, i: 0, o: 0, u: 0 };
+      let letter;
+      for (let i = 0; i < string.length; i++) {
+         letter = string[i];
+         if ((Object.keys(vowelsResult)).includes(letter)) {
+            vowelsResult[letter] = vowelsResult[letter] + 1;
+         }
+      }
+      return vowelsResult;
+   } else {
+      throw new Error("Invalid Input");
+   }
+};
 
 module.exports = frequencyCounter;
