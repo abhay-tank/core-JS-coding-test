581580450bf45d4806abeb3b79a75318f2820a25
abhay-tank <abhay.tank@raweng.com>
2020-12-04 12:18:28 +0530

    Addition problem solved

--- questions/addition.js
+++ questions/addition.js
@@ -5,7 +5,17 @@
 */
 
 const addition = (...numbers) => {
-  // all your code
+  if (numbers.length) {
+    return numbers.reduce((sum, currentNumber) => {
+      if (typeof currentNumber == "number" && isFinite(currentNumber)) {
+        return sum + currentNumber;
+      } else {
+        throw new Error("Invalid Input");
+      }
+    });
+  } else {
+    throw new Error("Invalid Input");
+  }
 };
 
 module.exports = addition;
